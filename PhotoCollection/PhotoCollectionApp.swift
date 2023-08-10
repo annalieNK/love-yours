6f4a4e0e042397cbe163ebadde0e0c15be244efa
Annalie <annaliakruseman@gmail.com>
2023-08-10 15:08:23 -0700

    Initial Commit

---
+++ PhotoCollection/PhotoCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionApp.swift
+//  PhotoCollection
+//
+//  Created by Annalie Kruseman on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
